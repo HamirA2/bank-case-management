cb03c765b96233507060b3e6371604cd2b7204ad
Hamir Aquino <hamiraquino@MacBookAir.lan>
2025-08-20 15:20:17 -0400

    started bank case management

---
+++ src/test/java/com/bank/case_management/CaseManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.case_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
